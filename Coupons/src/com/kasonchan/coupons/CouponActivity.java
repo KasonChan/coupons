9b33b0a89d8f80a80600b34111153eba00e304ff
Kason <kasonl.chan@gmail.com>
2015-04-02 04:17:15 -0500

    Added json parsing in coupons, coupons layout, and GridAdaptor; edited CouponActivity, coupon layout

--- Coupons/src/com/kasonchan/coupons/CouponActivity.java
+++ Coupons/src/com/kasonchan/coupons/CouponActivity.java
@@ -3,17 +3,23 @@ package com.kasonchan.coupons;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.TextView;
 
 @SuppressWarnings("deprecation")
@@ -23,19 +29,23 @@ public class CouponActivity extends Activity {
     super.onCreate(savedInstanceState);
 
     // Display coupon layout
-    setContentView(R.layout.coupon);
+    setContentView(R.layout.coupons);
 
-    TextView couponUsername = (TextView) findViewById(R.id.coupon_username);
+    TextView couponUsername = (TextView) findViewById(R.id.coupons_username);
 
     // Get username from login or signup page
-    String username = this.getIntent().getStringExtra("username");
+    try {
+      String username = this.getIntent().getStringExtra("username");
 
-    // If username is not passed from previous intent, hide the username
-    // Otherwise, show username
-    if (username.isEmpty()) {
-      couponUsername.setVisibility(View.GONE);
-    } else {
-      couponUsername.setText("@" + username);
+      // If username is not passed from previous intent, hide the username
+      // Otherwise, show username
+      if (username.isEmpty()) {
+        couponUsername.setVisibility(View.GONE);
+      } else {
+        couponUsername.setText("@" + username);
+      }
+    } catch (Exception e) {
+
     }
 
     // Resource
@@ -53,7 +63,7 @@ public class CouponActivity extends Activity {
    */
   private class GetRequest extends AsyncTask<String, Void, String> {
 
-    TextView coupon = (TextView) findViewById(R.id.coupon_text);
+    // TextView coupon = (TextView) findViewById(R.id.coupon_text);
 
     @Override
     protected String doInBackground(String... args) {
@@ -90,8 +100,117 @@ public class CouponActivity extends Activity {
     }
 
     protected void onPostExecute(String result) {
-      // After get request is executed, set coupon text to result
-      coupon.setText(result);
+
+      final String PROMOTIONS = "promotions";
+      final String SAVINGS = "savings";
+      final String PERCENT_OFF = "percentOff";
+      final String AMOUNT_OFF = "amountOff";
+      final String TYPES = "types";
+      final String MERCHANT = "merchant";
+      final String MERCHANT_NAME = "name";
+      final String FOLLOWS_COUNT = "followsCount";
+      final String DESCRIPTION = "description";
+
+      ArrayList<HashMap<String, String>> couponList = new ArrayList<HashMap<String, String>>();
+      ;
+      JSONObject merchant;
+      String merchantName;
+      JSONObject savings;
+      Double percentOff;
+      Double amountOff;
+      JSONArray typeArray;
+      String types;
+      String description;
+      int followsCount;
+
+      try {
+        JSONObject jsonObj = new JSONObject(result);
+
+        JSONArray coupons = jsonObj.getJSONArray(PROMOTIONS);
+
+        for (int i = 0; i < coupons.length() - 1; i++) {
+          JSONObject coupon = coupons.getJSONObject(i);
+
+          // Get merchant name
+          merchant = coupon.getJSONObject(MERCHANT);
+          merchantName = merchant.getString(MERCHANT_NAME);
+
+          // Get followsCount
+          followsCount = coupon.getInt(FOLLOWS_COUNT);
+
+          // Get coupon description
+          savings = coupon.getJSONObject(SAVINGS);
+
+          if (savings.has(PERCENT_OFF) == true) {
+            // If percentOff exists, save it and types
+            percentOff = savings.getDouble(PERCENT_OFF);
+
+            description = percentOff + "%" + " Off";
+
+            // Create temp coupon
+            HashMap<String, String> tempCoupon = new HashMap<String, String>();
+
+            // Save json datas to new coupon
+            tempCoupon.put(MERCHANT_NAME, merchantName);
+            tempCoupon.put(DESCRIPTION, description);
+            tempCoupon.put(FOLLOWS_COUNT, Integer.toString(followsCount));
+
+            // Save temp coupon to the coupon list
+            couponList.add(tempCoupon);
+
+          } else if (savings.has(AMOUNT_OFF) == true) {
+            // If amountOff exists, save it and types
+            amountOff = savings.getDouble(AMOUNT_OFF);
+
+            description = "$" + amountOff + " Off";
+
+            // Create temp coupon
+            HashMap<String, String> tempCoupon = new HashMap<String, String>();
+
+            // Save json datas to new coupon
+            tempCoupon.put(MERCHANT_NAME, merchantName);
+            tempCoupon.put(DESCRIPTION, description);
+            tempCoupon.put(FOLLOWS_COUNT, Integer.toString(followsCount));
+
+            // Save temp coupon to the coupon list
+            couponList.add(tempCoupon);
+          } else {
+            // If the doesn't contain either of both
+            typeArray = savings.getJSONArray(TYPES);
+
+            types = "";
+            description = "";
+
+            for (int t = 0; t < typeArray.length(); t++) {
+              String type = typeArray.getString(t);
+              types = types + " " + type;
+            }
+
+            description = types;
+
+            // Create temp coupon
+            HashMap<String, String> tempCoupon = new HashMap<String, String>();
+
+            // Save json datas to new coupon
+            tempCoupon.put(MERCHANT_NAME, merchantName);
+            tempCoupon.put(DESCRIPTION, description);
+            tempCoupon.put(FOLLOWS_COUNT, Integer.toString(followsCount));
+
+            // Save temp coupon to the coupon list
+            couponList.add(tempCoupon);
+          }
+        }
+
+        // Show coupons
+        // Create grid adaptor and set coupon grid
+        GridAdaptor adapter = new GridAdaptor(CouponActivity.this, couponList);
+        GridView grid = (GridView) findViewById(R.id.coupons_grid);
+        grid.setAdapter(adapter);
+
+      } catch (JSONException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
     }
   }
 }
---
+++ Coupons/src/com/kasonchan/coupons/GridAdaptor.java
@@ -0,0 +1,86 @@
+package com.kasonchan.coupons;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class GridAdaptor extends BaseAdapter {
+  private Context                                  mContext;
+  private final ArrayList<HashMap<String, String>> coupons;
+
+  public GridAdaptor(Context c, ArrayList<HashMap<String, String>> coupons) {
+    mContext = c;
+    this.coupons = coupons;
+  }
+
+  @Override
+  public int getCount() {
+    return coupons.size();
+  }
+
+  @Override
+  public Object getItem(int position) {
+    return null;
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return 0;
+  }
+
+  @SuppressLint("InflateParams")
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+
+    final String MERCHANT_NAME = "name";
+    final String FOLLOWS_COUNT = "followsCount";
+    final String DESCRIPTION = "description";
+
+    View grid;
+    LayoutInflater inflater = (LayoutInflater) mContext
+        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+    if (convertView == null) {
+      grid = new View(mContext);
+      grid = inflater.inflate(R.layout.coupon, null);
+
+      TextView merchant = (TextView) grid.findViewById(R.id.coupon_merchant);
+      TextView description = (TextView) grid
+          .findViewById(R.id.coupon_description);
+      TextView followsCount = (TextView) grid
+          .findViewById(R.id.coupon_followsCount);
+      View shadow = (View) grid.findViewById(R.id.coupon_shadow);
+
+      String descriptionValue = coupons.get(position).get(DESCRIPTION);
+
+      merchant.setText(coupons.get(position).get(MERCHANT_NAME));
+      description.setText(descriptionValue);
+      followsCount.setText(coupons.get(position).get(FOLLOWS_COUNT));
+
+      // Set amount off shadow to red
+      // Set percent off shadow to blue
+      // Set others shows to yellow
+      if (descriptionValue.startsWith("$") && descriptionValue.endsWith(" Off")) {
+        shadow.setBackgroundColor(mContext.getResources().getColor(
+            R.color.red_accent_1));
+      } else if (descriptionValue.endsWith(" Off")) {
+        shadow.setBackgroundColor(mContext.getResources().getColor(
+            R.color.blue_accent_1));
+      } else {
+        shadow.setBackgroundColor(mContext.getResources().getColor(
+            R.color.yellow_accent_1));
+      }
+
+    } else {
+      grid = (View) convertView;
+    }
+    return grid;
+  }
+}
